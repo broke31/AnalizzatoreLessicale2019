6a3ab06d793fd7dad58a546ab4e6dad319035bf0
broke31 <giammagiorda@gmail.com>
2019-07-17 18:28:12 +0200

    first commit

---
+++ src/Tester.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+public class Tester {
+
+	public static void main(String[] args) throws IOException {
+
+		LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer();
+		String filePath = "inputFile.txt";
+		
+		if (lexicalAnalyzer.initialize(filePath)) {
+
+			Token token;
+			try {
+				while ((token = lexicalAnalyzer.nextToken()) != null) {
+					System.out.println(token.toString());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		} else
+			System.out.println("File not found!!");
+	
+	}
+
+}
